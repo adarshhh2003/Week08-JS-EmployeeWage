35004b7511156eaace3fbf4b7e042f4f1513540c
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2025-03-04 16:09:02 +0530

    [Adarsh] Add. calculating wages till working days and max days

--- EmoloyeeWage/employeewage.js
+++ EmoloyeeWage/employeewage.js
@@ -42,3 +42,17 @@ for(let day=0; day<NUM_OF_WORKING_DAYS; day++) {
 }
 empWage = empHrs * WAGE_PER_HOUR;
 console.log("Total Hrs: " + empHrs + " Emp Wage: " + empWage);
+
+
+const MAX_HOURS_IN_MONTH = 160;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while(totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    empCheck = Math.floor(Math.random()*10)%3;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+
+empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Days: " + totalWorkingDays + 
+    " Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
